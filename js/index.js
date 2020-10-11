76216de9a033a7df074d9aede001de2b802bbc0c
Josué de Paulo Viana <josuep.viana@gmail.com>
2020-10-11 02:36:52 -0300

    Nova versão
    Melhorias de design, logo nova, responsividade atualizada.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
-    $('.collapsible').collapsible();
-    $('.sidenav').sidenav();
-    $('.carousel').carousel();
+  $(".collapsible").collapsible();
+  $(".sidenav").sidenav();
+  $(".carousel").carousel();
 
-    var elem = document.querySelector('.collapsible.expandable');
-    var instance = M.Collapsible.init(elem, {
-        accordion: false
-    });
+  var elem = document.querySelector(".collapsible.expandable");
+  var instance = M.Collapsible.init(elem, {
+    accordion: false,
+  });
+
+  var btn = $("#button");
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
 });
