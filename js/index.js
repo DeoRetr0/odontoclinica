52ac1a55d6ed0007d1d5b4f288f15e5ac1048337
Josué de Paulo Viana <josuep.viana@gmail.com>
2020-10-11 22:58:25 -0300

    Versão final
    Form de agendamento e depoimentos de clientes adicionados.
    Design responsivo.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
+//materialize inits
   $(".collapsible").collapsible();
   $(".sidenav").sidenav();
   $(".carousel").carousel();
+  $('.modal').modal();
+  $('select').formSelect();
 
   var elem = document.querySelector(".collapsible.expandable");
   var instance = M.Collapsible.init(elem, {
     accordion: false,
   });
 
+// back to top
   var btn = $("#button");
 
   $(window).scroll(function () {
@@ -22,4 +26,24 @@ $(document).ready(function () {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "300");
   });
+
+
+// smooth scrolling
+$("a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
+  });
+});
+
+$('.main-gallery').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
 });
